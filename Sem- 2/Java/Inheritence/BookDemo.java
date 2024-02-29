3a0b1524381a4f9e5a6b9d337200bbbed29103ca
Bhoomi Tulsiyani <143326623+bhoomi1309@users.noreply.github.com>
2024-02-29 22:29:43 +0530

    Update BookDemo.java

--- Sem- 2/Java/Inheritence/BookDemo.java
+++ Sem- 2/Java/Inheritence/BookDemo.java
@@ -1,13 +1,16 @@
 public class BookDemo{
     public static void main(String[] args) {
-        Book b=new Book();
-        Book.book_publication book=new Book.book_publication();
-        Book.paper_publication paper=b.new paper_publication();
-        b.set_authorname(args[0]);
-        System.out.println("Author name: "+b.get_authorname());
-        book.set_book_publication(args[1]);
-        System.out.println("Book publication title: "+book.get_book_publication());
-        paper.set_paper_publication(args[2]);
-        System.out.println("Paper publication title: "+paper.get_paper_publication());
+        Book b;
+        if("book".equals(args[2])){
+            b=new book_publication(args[0],args[1]);
+        }
+        else if("paper".equals(args[2])){
+            b=new paper_publication(args[0],args[1]);
+        }
+        else{
+            System.out.println("Invalid publication type");
+            return;
+        }
+        b.print();
     }
 }
