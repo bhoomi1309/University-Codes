4ff305d024a64ded60176e39f88e2f4753fdbf43
Bhoomi Tulsiyani <143326623+bhoomi1309@users.noreply.github.com>
2024-06-23 01:25:50 +0530

    Update PascalTriangle.java

--- Sem- 3/Data Structure/Array/PascalTriangle.java
+++ Sem- 3/Data Structure/Array/PascalTriangle.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 public class PascalTriangle{
-	public static int factorial(int i){
-        if (i==0){
-            return 1;
-        }
-        return i*factorial(i-1);
-    }
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Pascal's Triangle");
 		System.out.print("Enter no. of rows to print: ");
 		int n=sc.nextInt();
-        for (int i=0;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                 System.out.print(" ");
-            }
-            for(int j=0;j<=i;j++){
-                 System.out.print(" "+ factorial(i)/(factorial(i - j)* factorial(j)));
-            }
-            System.out.println();
-        }
+        	int[][] arr=new int[n+1][n+1];
+        	for (int i=0;i<n;i++){
+            		for(int j=0;j<n-i;j++){
+                		System.out.print(" ");
+            		}
+            		for(int j=0;j<=i;j++){
+                		if(j==0||j==i){
+                    			arr[i][j]=1;
+                		}else{
+                    			arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                		}
+                		System.out.print(arr[i][j]+" ");
+            		}
+            		System.out.println();
+        	}
+        	sc.close();
 	}
 }
