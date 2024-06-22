675d0300ee1e3701ed84c732ea2b70967db852ea
Bhoomi Tulsiyani <143326623+bhoomi1309@users.noreply.github.com>
2024-06-23 01:31:30 +0530

    Update PascalTriangle.java

--- Sem- 3/Data Structure/Array/PascalTriangle.java
+++ Sem- 3/Data Structure/Array/PascalTriangle.java
@@ -5,7 +5,7 @@ public class PascalTriangle{
 		System.out.println("Pascal's Triangle");
 		System.out.print("Enter no. of rows to print: ");
 		int n=sc.nextInt();
-        	int[][] arr=new int[n+1][n+1];
+        	int[][] arr=new int[n][n];
         	for (int i=0;i<n;i++){
             		for(int j=0;j<n-i;j++){
                 		System.out.print(" ");
